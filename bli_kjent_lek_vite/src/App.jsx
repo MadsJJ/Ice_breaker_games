c074f7965bf09a5012208e93e864c44f1a5f1740
Mads <mjengesv@stud.ntnu.no>
2024-02-12 09:02:55 +0100

    made homepage

--- bli_kjent_lek_vite/src/App.jsx
+++ bli_kjent_lek_vite/src/App.jsx
@@ -1,17 +1,86 @@
-import { useState } from "react";
 import "./App.css";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+  CardActions,
+  Button,
+  CardMedia,
+} from "@mui/material";
+import Container from "@mui/material/Container";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const handleClick = () => {
+    console.log("Clicked");
+    location.href = "/src/";
+  };
+  const data = ["Lek 1", "Lek 2", "Lek 3", "Lek 4", "Lek 5", "Lek 6"];
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Container maxWidth="lg">
+        <Typography variant="h2" align="center">
+          KaldPrat
+        </Typography>
+        <Grid
+          container
+          maxWidth="900px"
+          justifyContent="center"
+          alignItems="center"
+          columns={18}
+          spacing={4}
+          style={{ marginTop: "50px" }}
+        >
+          {data.map((item, index) => (
+            <Grid item xs={3} sm={6} ms={3} key={index}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  minWidth: 250,
+                  borderRadius: "15px",
+                }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    sx={{ height: "140px" }}
+                    image="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"
+                    title="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    onClick={handleClick}
+                    color="secondary"
+                    variant="contained"
+                    size="small"
+                  >
+                    Button
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Button
+        id="newGameButton"
+        color="primary"
+        variant="contained"
+        size="large"
+      >
+        New Game
+      </Button>
     </>
   );
 }
