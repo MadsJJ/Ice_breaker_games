5257dc6fd2aad5a82c6144ab0fdbfc4ee50ada22
jenny Thonstad <jennyfth@ntnu.no>
2024-02-12 09:25:52 +0100

    laget login

---
+++ bli_kjent_lek_vite/src/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Login.css'
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Her kan du legge til logikken for å håndtere innsendingen av brukernavn og passord
+    console.log('Brukernavn:', username);
+    console.log('Passord:', password);
+    // For eksempel kan du her kalle en funksjon for å validere og sende dataene til en server
+  };
+
+  return (
+    <body>
+    <div className = 'loginContainer'>
+    <div className = 'loginboks'>
+    
+      <h2 class ='overskrift'>Logg inn</h2>
+      <form onSubmit={handleSubmit}>
+        <div className = 'input-boks'>
+          <label htmlFor="username">Brukernavn:</label>
+          <br></br>
+
+          <input
+            type="text"
+            placeholder='Skriv ditt brukernavn her ...'
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <br></br>
+
+        <div className = 'input-boks'>
+          <label htmlFor="password">Passord:</label>
+          <br></br>
+
+          <input
+            type="password"
+            placeholder='Skriv ditt brukernavn her ...'
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Logg inn</button>
+      </form>
+    </div>
+    </div>
+    </body>
+  );
+}
+
+export default Login;
