0bc03757fc670f032cd23d28b375347fa29289fd
jenny Thonstad <jennyfth@ntnu.no>
2024-02-14 13:28:49 +0100

    make new game page file

---
+++ bli_kjent_lek_vite/src/VisitGame.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/navbar'
+import './VisitGame.css'
+
+function VisitGame() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hei!</h1>
+    </>
+  )
+}
+
+export default VisitGame
--- bli_kjent_lek_vite/src/main.jsx
+++ bli_kjent_lek_vite/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from './Login.jsx'
 import './Login.css'
+import VisitGame from './VisitGame.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <VisitGame />
   </React.StrictMode>,
 )
 
