73938defadbf1e4ccfdd085c8d0136436a665f61
Edvard <edvaraa@ntnu.no>
2024-02-14 13:08:23 +0100

    chore: update file
    updated code for navbar component

--- bli_kjent_lek_vite/src/components/Navbar.jsx
+++ bli_kjent_lek_vite/src/components/Navbar.jsx
@@ -3,13 +3,18 @@ import './Navbar.css'
 export default function Navbar(){
     return(
         <>
-            <header>
+            <header className="navbar">
                 <a href="/"> {/* Empty link for now - will return you to homepage once routing logic is completed */}
                     <img src="" alt="KaldPrat logo" /> {/* Image tag for logo */}
                 </a>
-                <a href="/">Kategorier</a>
-                <button> Log in </button>
+                <nav className="navigation">
+                    <a href="/">Mine ratings</a>
+                    <a href="/">Mine spillelister</a>
+                    <a href="/about">Mine favoritter</a>
+                    <a href="/contact">Mine leker</a>
+                    <button href="/">Logg ut</button>
+                </nav>
             </header>
         </>
     )
-}
+} 
