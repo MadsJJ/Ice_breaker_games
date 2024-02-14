3ee7787213ac327e399021b56b342d96ea5f582a
Mads <mjengesv@stud.ntnu.no>
2024-02-14 13:56:59 +0100

    fixed correct login file

--- bli_kjent_lek_vite/src/login.jsx
+++ bli_kjent_lek_vite/src/login.jsx
@@ -1,117 +1,70 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import logo from "/logo.png";
+import { useState } from 'react'
+import kaldprat_logo from './assets/kaldprat_logo.png'
+import './style/Login.css'
 
-ReactDOM.createRoot(document.getElementById("root2")).render(
-  <React.StrictMode>
-    <SignIn />
-  </React.StrictMode>
-);
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-// TODO remove, this demo shouldn't need to reset the theme.
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
 
-const defaultTheme = createTheme();
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
 
-export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-    location.href = "../";
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Her kan du legge til logikken for å håndtere innsendingen av brukernavn og passord
+        console.log('Brukernavn:', username);
+        console.log('Passord:', password);
+        // For eksempel kan du her kalle en funksjon for å validere og sende dataene til en server
+    };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar
-            src={logo}
-            sx={{ width: 140, height: 140, m: 1, bgcolor: "secondary.main" }}
-          >
-            {/* <LockOutlinedIcon /> */}
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              InputLabelProps={{ required: false }}
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              InputLabelProps={{ required: false }}
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <body>
+            <div className='loginContainer'>
+                <div className='loginboks'>
+
+                    <div id="logoContainer">
+                        <img id="logo" src={kaldprat_logo} alt="error image" />
+                    </div>
+
+                    <h2 class='overskrift'>Logg inn</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className='input-boks'>
+                            <label htmlFor="username">Brukernavn:</label>
+                            <br></br>
+
+                            <input
+                                type="text"
+                                placeholder='Skriv ditt brukernavn her ...'
+                                id="username"
+                                value={username}
+                                onChange={handleUsernameChange}
+                            />
+                        </div>
+                        <br></br>
+
+                        <div className='input-boks'>
+                            <label htmlFor="password">Passord:</label>
+                            <br></br>
+
+                            <input
+                                type="password"
+                                placeholder='Skriv ditt brukernavn her ...'
+                                id="password"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                        </div>
+                        <button type="submit">Logg inn</button>
+                    </form>
+                </div>
+            </div>
+        </body>
+    );
 }
+
+export default Login;
