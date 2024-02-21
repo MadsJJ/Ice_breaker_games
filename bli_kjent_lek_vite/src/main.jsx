0de2c755907db087262ce36247ebc662c72777df
Edvard <edvaraa@ntnu.no>
2024-02-21 21:28:41 +0100

    chore: update file
    removed unused inputs and set root back to App

--- bli_kjent_lek_vite/src/main.jsx
+++ bli_kjent_lek_vite/src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NewGame from "./NewGame.jsx";
-import "./NewGame.css";
 import App from "./App.jsx";
+import "./style/App.css";
 import "./style/index.css";
-import UserData from "./readUserdata.jsx";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "./style/App.css";
-import Login from "./Login.jsx";
 
 export const theme = createTheme({
   palette: {
@@ -33,8 +29,7 @@ export const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <Login />
-      {/* <UserData /> */}
+      <App />
     </React.StrictMode>
   </ThemeProvider>
 );
