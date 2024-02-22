65e1d6bcbc39b6324caede98e2f5326ffc05d471
Edvard <edvaraa@ntnu.no>
2024-02-22 09:23:06 +0100

    fix: styling
    fixed styling for login page - added route between login and home with navbar

--- bli_kjent_lek_vite/src/Login.jsx
+++ bli_kjent_lek_vite/src/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import kaldprat_logo from "./assets/kaldprat_logo.png";
+import Navbar from "./components/Navbar";
 import "./style/Login.css";
 import { db } from "./firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
@@ -49,54 +50,54 @@ function Login() {
   };
 
   return (
-    <body className={styles.loginbody}>
-      <div className="loginContainer">
-        <div className={styles.loginboks}>
-          <div id={styles.logoContainer}>
-            <img id={styles.logo} src={kaldprat_logo} alt="error image" />
+    <>
+      <Navbar />
+      <div className="loginbody">
+        <div className="loginContainer">
+          <div className="loginboks">
+            <div id="logoContainer">
+              <img id="logo" src={kaldprat_logo} alt="error image" />
+            </div>
+
+            <h2 className="overskrift">Logg inn</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="inputboks">
+                <label htmlFor="username">Brukernavn:</label>
+
+                <input
+                  className="loginInput"
+                  type="text"
+                  placeholder="Skriv ditt brukernavn her ..."
+                  id="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </div>
+
+              <div className="inputboks">
+                <label htmlFor="password">Passord:</label>
+
+                <input
+                  className="loginInput"
+                  type="password"
+                  placeholder="Skriv ditt brukernavn her ..."
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <button
+                onClick={handleNavigate}
+                className="loginButton"
+                type="submit"
+              >
+                Logg inn
+              </button>
+            </form>
           </div>
-
-          <h2 className={styles.overskrift}>Logg inn</h2>
-          <form onSubmit={handleSubmit}>
-            <div className={styles.inputboks}>
-              <label htmlFor="username">Brukernavn:</label>
-              <br></br>
-
-              <input
-                className={styles.loginInput}
-                type="text"
-                placeholder="Skriv ditt brukernavn her ..."
-                id="username"
-                value={username}
-                onChange={handleUsernameChange}
-              />
-            </div>
-            <br></br>
-
-            <div className={styles.inputboks}>
-              <label htmlFor="password">Passord:</label>
-              <br></br>
-
-              <input
-                className={styles.loginInput}
-                type="password"
-                placeholder="Skriv ditt brukernavn her ..."
-                id="password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </div>
-            <button
-              onClick={handleNavigate}
-              className={styles.loginButton}
-              type="submit"
-            >
-              Logg inn
-            </button>
-          </form>
         </div>
       </div>
-    </body>
+    </>
   );
 }
 
