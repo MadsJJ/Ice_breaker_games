c6568a97d1d172b249798642513e1605a71b4a3b
Mads <mjengesv@stud.ntnu.no>
2024-02-27 17:49:07 +0100

    made a proof of concept how you can get logged in user 😑

--- bli_kjent_lek_vite/src/components/Navbar.jsx
+++ bli_kjent_lek_vite/src/components/Navbar.jsx
@@ -1,31 +1,55 @@
-import './style/Navbar.css'
-import kaldprat_logo from '../assets/kaldprat_logo.png'
+import "./style/Navbar.css";
+import kaldprat_logo from "../assets/kaldprat_logo.png";
 //routing
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function Navbar(){
-    //routing
+export default function Navbar() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    //ChatGPT
+    // Check if username exists in local storage
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
+  //routing
   let navigate = useNavigate();
 
-  const handleNavigate = () => {
-    navigate('/Login'); 
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/Login");
   };
 
-    return(
-        <>
-            <header className="navbar">
-                <a href="/"> {/* Empty link for now - will return you to homepage once routing logic is completed */}
-                    <img src={kaldprat_logo} alt="KaldPrat logo" width="80px" /> {/* Image tag for logo */}
-                </a>
-                <nav className="navigation">
-                    <a href="/">Mine ratings</a>
-                    <a href="/">Mine spillelister</a>
-                    <a href="/">Mine favoritter</a>
-                    <a href="/">Mine leker</a>
-                    <button onClick={handleNavigate}>Logg In</button>
-                </nav>
-            </header>
-        </>
-    )
-} 
+  const handleNavigate = () => {
+    navigate("/Login");
+  };
+
+  return (
+    <>
+      <header className="navbar">
+        <a href="/">
+          {" "}
+          {/* Empty link for now - will return you to homepage once routing logic is completed */}
+          <img src={kaldprat_logo} alt="KaldPrat logo" width="80px" />{" "}
+          {/* Image tag for logo */}
+        </a>
+        <nav className="navigation">
+          <a href="/">Mine ratings</a>
+          <a href="/">Mine spillelister</a>
+          <a href="/">Mine favoritter</a>
+          <a href="/">Mine leker</a>
+          <button>Hei {username || "Gjest"}, godt å se deg!</button>
+          {username ? (
+            <button onClick={handleLogout}>Logg ut</button>
+          ) : (
+            <button onClick={handleNavigate}>Logg inn</button>
+          )}
+        </nav>
+      </header>
+    </>
+  );
+}
